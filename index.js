cf024db453d26e9c5aa1447ed6c15f66e05e56a1
Joanne Imbisi <evizengwa@yahoo.com>
2022-07-10 19:54:32 -0700

    commit tree 2fa7b4289c9064c5a39fc623acc8fdfc2e8f3d33

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
-}
+  
+      for (let i = 0; i < array.length; i++) {
+        const comp = target - array[i];
+        for (let j = i + 1; j < array.length; j++) {
+          if (array[j] === comp) return true;
+        }
+      }
+    
+      return false;
+    }
+
 
 /* 
   Write the Big O time complexity of your function here
+  O(n^2)
 */
-
 /* 
   Add your pseudocode here
+One pair of numbers added together equal our target number
 */
 
 /*
